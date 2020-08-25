ae485917309fdb9562c0c122f79b1cb00bee0c03
mp371366 <mp371366@github.com>
2020-08-25 12:13:10 +0200

    Add csrf protection.

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as csrf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 import { getMostExpensive, getMeme } from './memes';
 
 const app = express();
@@ -9,6 +11,8 @@ app.locals.basedir = __dirname;
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
 
 app.get('/', (request, response) => {
   const mostExpensive = getMostExpensive();
@@ -17,15 +21,15 @@ app.get('/', (request, response) => {
 
 app.get('/meme/:memeId', (request, response) => {
   const meme = getMeme(request.params.memeId);
-  response.render('meme', { title: `Meme ${meme.name}`, meme })
+  response.render('meme', { title: `Meme ${meme.name}`, meme, csrfToken: request.csrfToken() });
 });
 
 app.post('/meme/:memeId', (request, response) => {
   const meme = getMeme(request.params.memeId);
   const price = request.body.price;
   meme.price = price;
-  response.render('meme', { title: `Meme ${meme.name}`, meme })
-})
+  response.render('meme', { title: `Meme ${meme.name}`, meme, csrfToken: request.csrfToken() });
+});
 
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
